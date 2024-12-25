41af675f2dd58f188074a3113fc86139982f39fe
jeku001 <jedslu@st.amu.edu.pl>
2024-12-25 03:10:16 +0100

    improved parser using pd.read_csv(usecols=)

--- tests/parserTest_usecol.py
+++ tests/parserTest_usecol.py
@@ -1,9 +1,7 @@
-import threading
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import os
 import time
 import pandas as pd
-
 
 class Parser:
     def __init__(self, base_folder, additional_columns=None, start_year=None, end_year=None, workers=1):
@@ -18,14 +16,16 @@ class Parser:
     @staticmethod
     def parse_single_file(file_path, required_columns):
         try:
+            # Use usecols to load only necessary columns
             df = pd.read_csv(file_path, encoding="iso-8859-1", usecols=required_columns)
-            return df
+            selected_data = df.sort_values(by=["'Date (J2000 mseconds)"], ascending=True)
+            return selected_data
         except Exception as e:
-            print(f"{threading.current_thread()}: Error processing file {file_path}: {e}")
+            print(f"Error processing file {file_path}: {e}")
             return pd.DataFrame(columns=required_columns)
 
     def parse_data_no_merging(self, file_pattern="0-Power Board", progress_callback=None):
-        self.start_time = time.time()
+        self.start_time = time.time()  # start time of the entire process
         required_columns = [
             "'Date (YYYY-MM-DD HH:MM:SS)",
             "'Date Millisecond Offset",
@@ -56,57 +56,50 @@ class Parser:
 
         scan_end_time = time.time()
         scan_duration = scan_end_time - scan_start_time
-        print(f"{threading.current_thread()}: File scanning completed in {scan_duration:.2f} seconds")
-        print(f"{threading.current_thread()}: Found {len(files_to_process)} files to process.")
-
+        print(f"File scanning completed in {scan_duration:.2f} seconds")
+        print(f"Found {len(files_to_process)} files to process.")
         data_list = []
         total_files = len(files_to_process)
 
-        # Use ThreadPoolExecutor if files are less than 500 or ProcessPoolExecutor if more,
-        # assuming multiprocessing is enabled
+        # Use ThreadPoolExecutor or ProcessPoolExecutor based on file count and whether multiprocessing is enabled
         if self.workers > 1:
-            if total_files < 500:
-                executor_class = ThreadPoolExecutor
-                print(f"{threading.current_thread()}: Less than 500 files to process, using ThreadPoolExecutor for parsing...")
-                print(f"{threading.current_thread()}: {self.workers} parallel tasks ")
-            else:
-                executor_class = ProcessPoolExecutor
-                print(f"{threading.current_thread()}: More than 500 files to process, using ProcessPoolExecutor for parsing...")
-                print(f"{threading.current_thread()}: {self.workers} parallel tasks ")
+            executor_class = ThreadPoolExecutor if total_files < 500 else ProcessPoolExecutor
+            print(f"Using {executor_class.__name__} with {self.workers} parallel tasks.")
 
-            processed_count = 0
             with executor_class(max_workers=self.workers) as executor:
                 results = executor.map(self.parse_single_file, files_to_process, [required_columns] * total_files)
                 for res_df in results:
                     if not res_df.empty:
                         data_list.append(res_df)
-                    processed_count += 1
-                    # Call the progress callback after each file is processed
-                    if progress_callback:
-                        progress_callback(processed_count, total_files)
-                    # print(f"Processed {processed_count}/{total_files} files...")
+
         else:
-            print("{threading.current_thread()}: Using single-threaded parsing...")
-            processed_count = 0
+            print("Using single-threaded parsing...")
             for fpath in files_to_process:
                 res_df = self.parse_single_file(fpath, required_columns)
                 if not res_df.empty:
                     data_list.append(res_df)
-                processed_count += 1
-                if progress_callback:
-                    progress_callback(processed_count, total_files)
-                # print(f"Processed {processed_count}/{total_files} files...")
 
         if data_list:
             long_df = pd.concat(data_list, ignore_index=True)
         else:
             long_df = pd.DataFrame(columns=required_columns)
-        self.end_time = time.time()
+
         long_df = long_df.sort_values(by=["'Date (J2000 mseconds)"], ascending=True)
         if "'Date (YYYY-MM-DD HH:MM:SS)" in long_df.columns:
             long_df["'Date (YYYY-MM-DD HH:MM:SS)"] = pd.to_datetime(long_df["'Date (YYYY-MM-DD HH:MM:SS)"])
 
+        self.end_time = time.time()
         total_duration = self.end_time - self.start_time
-        print(f"{threading.current_thread()}: Processing completed in {total_duration:.2f} seconds")
+        print(f"Processing completed in {total_duration:.2f} seconds (including file scanning).")
 
         return long_df
+
+if __name__ == "__main__":
+    parser = Parser("G:\\CBK_windows_test_package",
+        ["'Wheel Sum Current(mA)"],
+        start_year=None,
+        end_year=None,
+        workers=8)
+    for i in range(20):
+        parsed_data = parser.parse_data_no_merging()
+        print(f"{i}")
